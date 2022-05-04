e1a63a162061ecaea81406d16f6f66b13819e895
ganzhangfan <735180443@qq.com>
2022-05-04 17:39:29 +0800

    初始化

---
+++ src/components/box.jsx
@@ -0,0 +1,16 @@
+import React,{Component}from 'react';
+
+export default  class Box extends Component{
+    //存数据 也可以再建一个
+    state={}
+
+    render(){
+        return (
+            <React.Fragment>
+               
+                <h1>hello world</h1>;
+
+            </React.Fragment>   
+        );
+    }
+}
